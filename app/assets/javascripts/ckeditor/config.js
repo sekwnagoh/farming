8045e181dc383e22af89dad19c4d32a3bb132904
parkhaeun <syhe617@gmail.com>
2016-07-21 15:15:54 +0900

    씨케이에디터추가 및 카데고리이상해짐

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,8 @@
+CKEDITOR.editorConfig = function (config) {
+	// ... other configuration ...
+	config.toolbar = 'simple';
+	config.toolbar_mini = [
+		["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
+	]; 
+	// ... rest of the original config.js  ...
+}
--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,4 +1,7 @@
 Ckeditor.setup do |config|
   # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = "//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"
+ 	config.cdn_url = "//cdn.ckeditor.com/4.5.10/standard/ckeditor.js"
+	config.assets_languages = ['en', 'fr']
+  	config.assets_plugins = ['image', 'smiley']
+
 end
